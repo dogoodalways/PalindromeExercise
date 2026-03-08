21f5f3f96ebd1451b67d5958e5cc99428329d9ab
Yosef Abramov <yosefamiami@gmail.com>
2026-03-08 09:35:59 +0200

    kalabanga

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            return false;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,28 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
 
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("Racecar", true)]
+        public void PalindromeTester(string word, bool expected)
+        {
+            // Arrange
+            WordSmith smith = new WordSmith();
+
+            // Act
+            bool actual = smith.IsAPalindrome(word);
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
+
 }
+    
